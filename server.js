7d87e7c05e8918d2a2cfb607014abe226a212330
Zef Hemel <zef@zef.me>
2014-08-14 11:24:43 +0200

    Initial version

---
+++ server.js
@@ -0,0 +1,85 @@
+var http = require("zed/http");
+var fs = require("zed/fs");
+var ui = require("zed/ui");
+
+var mimeTypes = require("./mime_types");
+// TODO: make this configurable
+var prefix = "";
+
+function getContentType(path) {
+    var parts = path.split('.');
+    var ext = parts[parts.length - 1];
+    return mimeTypes[ext] || "application/octet-stream";
+}
+
+function serveFileListing() {
+    return fs.listFiles().then(function(allFiles) {
+        var html = "<body><h1>File listing</h1><ul>";
+        allFiles.forEach(function(path) {
+            html += "<li><a href='" + path + "'>" + path + "</a></li>";
+        });
+        html += "<ul></body>";
+        return {
+            status: 200,
+            headers: {
+                "Content-Type": "text/html",
+            },
+            body: html
+        };
+    });
+}
+
+function requestHandler(req) {
+    var path = req.path;
+    return fs.readFile(prefix + path, true).then(function(content) {
+        return {
+            status: 200,
+            headers: {
+                "Content-Type": getContentType(path)
+            },
+            body: content
+        };
+    }, function() {
+        // We'll assume a not found error. Let's try one more thing: adding /index.html to the end
+        if (path[path.length - 1] === "/") {
+            path = path.substring(0, path.length - 1);
+        }
+
+        return fs.readFile(prefix + path + "/index.html", true).then(function(content) {
+            return {
+                status: 200,
+                headers: {
+                    "Content-Type": "text/html"
+                },
+                body: content
+            };
+        }, function() {
+            if (path === "/" || path === "") {
+                return serveFileListing();
+            }
+            return {
+                status: 404,
+                headers: {
+                    "Content-Type": "text/plain"
+                },
+                body: "Not found"
+            };
+        });
+    });
+}
+
+module.exports = function(info) {
+    switch (info.action) {
+        case 'start':
+            http.startServer("staticserver", "Tools:Static Server:Request").then(function(url) {
+                console.log("Listening on", url);
+                ui.openUrl(url);
+            });
+            break;
+        case 'request':
+            return requestHandler(info.request);
+        case 'stop':
+            http.stopServer("staticserver");
+            break;
+    }
+};
